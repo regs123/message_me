c563c7f33d4f4e64fd6fd36374e0344644e42943
Regie_Zuitt <parasregienald@gmail.com>
2020-04-06 02:15:10 -1000

    add favicon and navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(document).on('turbolinks:load', function() {
+    $('.ui.dropdown').dropdown();
+})
