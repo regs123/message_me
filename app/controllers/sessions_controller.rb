25e4111961fa60135d239e6b18dd4aaa568a5364
Regie_Zuitt <parasregienald@gmail.com>
2020-04-09 01:03:53 -1000

    add restriction on views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
 
+    before_action :logged_in_redirect, only: [:new, :create]    
+    
     def new
     end
 
@@ -19,5 +21,14 @@ class SessionsController < ApplicationController
         session[:user_id] = nil
         flash[:success] = "Logout"
         redirect_to login_path
+    end
+
+    private
+
+    def logged_in_redirect
+        if logged_in?
+            flash[:error] = "You already logged in"
+            redirect_to root_path
+        end
     end
 end
